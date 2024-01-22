278ae49518394e7d669e0a5720f8a7732a2f50b7
Guenael Labois <guenael.labois@devsavant.ai>
2024-01-21 20:05:58 -0500

    use literal state to avoid reference array copy

--- frontend/js/components/board.js
+++ frontend/js/components/board.js
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
 import { Cell } from "../components/cell";
 export const Board = () => {
-  const [board, setBoard] = useState(Array(9).fill(Array(9).fill("0")));
+  const [board, setBoard] = useState(
+    [
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"],
+      ["0","0","0","0","0","0","0","0","0"]
+    ]
+  );
 
   let newBoard = board.map((row, rowIndex) => {
     return row.map((col, colIndex) => {
